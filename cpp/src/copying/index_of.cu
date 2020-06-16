18d3fca4bab83391dd7b075abd902f50d18b9202
Ashwin Srinath <shwina@users.noreply.github.com>
2020-06-16 14:32:10 -0400

    Include upper bound in index_of

--- cpp/src/copying/index_of.cu
+++ cpp/src/copying/index_of.cu
@@ -22,6 +22,8 @@
 
 #include <numeric>
 
+#include <thrust/transform_scan.h>
+
 namespace cudf {
 namespace detail {
 
@@ -46,24 +48,55 @@ std::unique_ptr<column> index_of(
   auto index_column = sorted_key_table->view().column(0);
   auto search_table = sorted_key_table->view().select(column_selection);
 
-  auto lb        = lower_bound(search_table,
+  auto lb = lower_bound(search_table,
                         keys,
                         std::vector<order>(search_table.num_columns(), order::ASCENDING),
                         std::vector<null_order>(search_table.num_columns(), null_order::AFTER),
                         mr,
                         stream);
-  auto d_idx_ptr = column_device_view::create(index_column, stream);
-  auto d_idx     = *d_idx_ptr;
+
+  auto ub = upper_bound(search_table,
+                        keys,
+                        std::vector<order>(search_table.num_columns(), order::ASCENDING),
+                        std::vector<null_order>(search_table.num_columns(), null_order::AFTER),
+                        mr,
+                        stream);
+
+  // calculate size of the result
+  rmm::device_vector<size_type> offsets(lb->size() + 1, 0);
+  thrust::transform_inclusive_scan(
+    thrust::device,
+    thrust::make_zip_iterator(
+      thrust::make_tuple(lb->view().begin<size_type>(), ub->view().begin<size_type>())),
+    thrust::make_zip_iterator(
+      thrust::make_tuple(lb->view().end<size_type>(), ub->view().end<size_type>())),
+    offsets.begin() + 1,
+    [] __device__(thrust::tuple<size_type, size_type> elem) -> size_type {
+      return thrust::get<1>(elem) - thrust::get<0>(elem);
+    },
+    thrust::plus<size_type>());
+
+  auto result_size = (size_type)offsets[lb->size()];
 
   auto result = cudf::make_numeric_column(
-    data_type(type_to_id<size_type>()), keys.num_rows(), mask_state::UNALLOCATED, stream, mr);
-  auto d_result = result.get()->mutable_view();
+    data_type(type_to_id<size_type>()), result_size, mask_state::UNALLOCATED, stream, mr);
 
-  thrust::transform(rmm::exec_policy()->on(stream),
-                    lb->view().begin<size_type>(),
-                    lb->view().end<size_type>(),
-                    d_result.begin<size_type>(),
-                    [d_idx] __device__(size_t index) { return d_idx.element<size_type>(index); });
+  auto d_result = result->mutable_view().begin<size_type>();
+  auto d_idx    = index_column.begin<size_type>();
+
+  thrust::for_each_n(
+    rmm::exec_policy()->on(stream),
+    thrust::make_zip_iterator(thrust::make_tuple(
+      lb->view().begin<size_type>(), ub->view().begin<size_type>(), offsets.begin())),
+    result_size,
+    [d_idx, d_result] __device__(thrust::tuple<size_type, size_type, size_type> index_tup) {
+      thrust::for_each(thrust::seq,
+                       thrust::make_counting_iterator(thrust::get<0>(index_tup)),
+                       thrust::make_counting_iterator(thrust::get<1>(index_tup)),
+                       [d_idx, d_result, index_tup] __device__(size_type i) {
+                         *(d_result + thrust::get<2>(index_tup)) = *(d_idx + i);
+                       });
+    });
   return result;
 }
 
